10a81f08c1f66f4f77d8ec585c71cdb1b213d599
Rory Poonyth <66537306+RoryPoonyth@users.noreply.github.com>
2024-11-07 06:47:33 +0000

    Cleanup

--- CODE/enums/SpaceType.java
+++
@@ -1,7 +0,0 @@
-package enums;
-
-public enum SpaceType {
-    Gym,
-    Room,
-    SwimmingPool
-}
--- CODE/model/Guardian.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-import java.util.List;
-
-//public class Guardian extends User {
-//Implementation of the guardian class
-//  private List<Client> children;
-//}
